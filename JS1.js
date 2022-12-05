273cdd18a2e7466731031ab812b89e633eaa44a1
robert <robertcioltea@gmail.com>
2022-12-05 19:38:17 +0200

    first js approach

---
+++ JS1.js
@@ -0,0 +1,18 @@
+//console.log('Hello World!')
+//data types: undefined ,null, boolean, string , symbol ,number, object
+
+
+var number = 5 //in tot programul
+let nume = "mihai"//va fi folosita in doar in locul in care este declarata
+const pi =3.14 // o variabila care nu se mai schimba
+
+
+var a=10;
+var b=2;
+
+console.log(`The product is ${a*b}`)
+console.log(`The division is ${a/b}`)
+console.log(`The remainder is ${a%b}`)
+console.log(`The sum is ${a+b}`)
+console.log(`The diffrence is ${a-b}`)
+console.log(`a decremented by 1 is ${--a}`)
